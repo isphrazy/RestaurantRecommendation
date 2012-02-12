3dd62da0954e1e67d34f2a7e0a0a504a613502f7
Pingyang He <pingyh@cs.washington.edu>
2012-02-12 11:50:06 -0800

    search api finished

--- api_search.php
+++ api_search.php
@@ -1,24 +1,52 @@
 <?php
 	
-/*
-	include 'result.php';
-*/
+	include '../search.php';
+	
+	define('NO_RESTAURANT_FOUND_MESSAGE', -1);
+	define('MANY_RESTAURANTS_FOUND_MESSAGE', 0);
 
-	$restaurants_basic_info_json;
-	$favorite_restaurants_weight;
-	$new_restaurant_name;
-	$found;
-	
-/*
 	get_r_name();
-*/
-	$new_restaurant_name = $_REQUEST["r_name"];
-	$new_restaurant_name = trim($new_restaurant_name);
-	//if it's true, then this call is by this file itself, and $new_restaurant_name
-	//is the key of the restaurant.
-	//if not set, then we will search restaurant name to find the restaurant.
-	$found = isset($_REQUEST["sure"]);
 	
-	print($new_restaurant_name);
+	if(!$found){
+		$nFound = 0;
+		if($new_restaurant_name != ''){
+			
+			$search_result = search_restaurant($new_restaurant_name);
+
+			$nFound = count($search_result);
+		}
+		if($nFound === 0){
+			print(NO_RESTAURANT_FOUND_MESSAGE);
+		}else if($nFound === 1){
+			$found = true;
+			$name_array = array_keys($search_result);
+			$new_restaurant_name = $name_array[0];
+		}else{//found several restaurant
+			print(json_encode(array(MANY_RESTAURANTS_FOUND_MESSAGE, $search_result)));
+		}
+	}
+	
+	if($found){
+		
+		$restaurants_basic_info_json = json_decode(file_get_contents(RESTAURANT_BASIC_DATA_FILE), true);
+		
+		$restaurant_basic_info = $restaurants_basic_info_json[$new_restaurant_name];
+		
+		$new_f_restaurant = generate_favorite_restaurant($restaurant_basic_info);
+		
+		//this list contains user's favorite restaurants
+		$favorite_restaurants_list = add_to_favorite_restaurants_list($new_f_restaurant);
+			
+		//the list contains relevant restaurants
+		$relevant_restaurants_list = generate_relevant_restaurants_list($favorite_restaurants_list);
+
+		//sorts the relevant restaurants list based on relevance, qualities, and price 
+		usort($relevant_restaurants_list, 'cmp');
+
+		//prints the relevant restaurants list.
+		print(json_encode($relevant_restaurants_list));
+		
+	}
+	
 
 ?>
