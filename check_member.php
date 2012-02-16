004c001e2773f0c9a74252771aac51a8e0aa7134
Jinghao Liu <liujh@cs.washington.edu>
2012-02-15 18:28:10 -0800

    Update check_member.php

--- check_member.php
+++ check_member.php
@@ -1,5 +1,5 @@
 <?php
-
+	include 'connect_db.php';
 	define('USERNAME_Q', 'myusername');
 	define('PASSWORD_Q', 'mypassword');
 
@@ -10,15 +10,7 @@
 		global $myusername;
 		global $mypassword;
 		
-		$host="mysql17.000webhost.com"; // Host name
-		$username="a6591147_jinghao"; // Mysql username
-		$password="admin123"; // Mysql password
-		$db_name="a6591147_mydata"; // Database name
-		$tbl_name="members"; // Table name
-
-		// Connect to server and select databse.
-		mysql_connect("$host", "$username", "$password")or die("cannot connect");
-		mysql_select_db("$db_name")or die("cannot select DB");
+		connect_db();
 
 		// To protect MySQL injection
 		$myusername = stripslashes($myusername);
@@ -26,7 +18,7 @@
 		$myusername = mysql_real_escape_string($myusername);
 		$mypassword = mysql_real_escape_string($mypassword);
 
-		$sql="SELECT * FROM $tbl_name WHERE username='$myusername' AND password='$mypassword'";
+		$sql="SELECT * FROM members WHERE username='$myusername' AND password='$mypassword'";
 		return mysql_query($sql); // result
 	}
 ?>
