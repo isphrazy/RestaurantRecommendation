a162fc89fb0b809cea69a9cafb11270dfba4489c
Jinghao Liu <liujh@cs.washington.edu>
2012-02-15 20:06:20 -0800

    Update is_like.php

--- is_like.php
+++ is_like.php
@@ -1,29 +1,21 @@
 <?php
-	include 'connect_db.php';
+	include 'background_profile.php';
 	session_start();
 
 	$rid = $_POST["rid"];
 	$is_like = $_POST["like"];
 	
-	connect_db();
+	connect_db_and_define_access_token();
 	
-	// Define $access_token
-	if ( isset($_SESSION['SESS_ACCESS_TOKEN']) ) {
-		$access_token=$_SESSION['SESS_ACCESS_TOKEN'];
-	} else {
-		$access_token=$_REQUEST["access_token"]; // for Android
+	$getid="SELECT id FROM members WHERE access_token='$access_token'";
+	$result = mysql_query($getid);
+	$row = mysql_fetch_assoc($result);
+	$uid = $row['id'];
+	
+	if ($is_like==1) {
+		$sql="INSERT likes VALUES('$uid','$rid')";
+	} elseif ($is_like==0) {
+		$sql="DELETE FROM likes WHERE uid='$uid' AND rid='$rid'";
 	}
-	if ( !empty($access_token) ) {
-		$getid="SELECT id FROM members WHERE access_token='$access_token'";
-		$result = mysql_query($getid);
-		$row = mysql_fetch_assoc($result);
-		$uid = $row['id'];
-		
-		if ($is_like==1) {
-			$sql="INSERT likes VALUES('$uid','$rid')";
-		} elseif ($is_like==0) {
-			$sql="DELETE FROM likes WHERE uid='$uid' AND rid='$rid'";
-		}
-		mysql_query($sql);
-	}
+	mysql_query($sql);
 ?>
