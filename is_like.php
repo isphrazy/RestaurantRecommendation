0378815a759af1ea67df6209c747fd4dd4c05323
Jinghao Liu <liujh@cs.washington.edu>
2012-02-16 00:25:03 -0800

    Update is_like.php

--- is_like.php
+++ is_like.php
@@ -7,18 +7,13 @@
 
 	connect_db_and_define_access_token();
 
-	if ($is_like===1) {
+	if ($is_like==1) {
 		$sql = "INSERT likes
 				VALUES((SELECT id FROM members WHERE access_token='$access_token'),'$rid')";
-	} elseif ($is_like===0) {
+	} elseif ($is_like==0) {
 		$sql = "DELETE FROM likes
 				WHERE uid=(SELECT id FROM members WHERE access_token='$access_token')
 				AND rid='$rid'";
 	}
-<<<<<<< HEAD
-	mysql_query($sql);
-?>
-=======
 	return mysql_query($sql);
 ?>
->>>>>>> 872712b74cffd057965f7eec9928e5c061f326ab
